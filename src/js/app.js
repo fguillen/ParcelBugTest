e147eed50fb40c65f27cc973d60565fa823be123
Fernando Guillen <fguillen.mail@gmail.com>
2023-10-16 11:59:54 +0200

    Bug reproduced

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+class App {
+  constructor() {
+    this.class_1 = new Class1();
+  }
+}
+
+new App();
---
+++ src/js/class_1.js
@@ -0,0 +1,5 @@
+class Class1 {
+  constructor() {
+    console.log("Class1 constructor()");
+  }
+}
